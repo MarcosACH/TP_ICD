c6f11f2caade413479d51fc3ad33f30f08133be7
Marcos Achaval <marcos.achavalr@gmail.com>
2024-05-15 17:42:38 -0300

    First commit

---
+++ analisis_car_prices.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(modelr)
+
+df = read_csv("car_prices.csv")
+
+# Exploracion de df
+nas = summary(df)
+nas
+
+# Chequeo de errores estructurales
+glimpse(df)
+# year: dbl --> int
+# condition: dbl --> int
+# odometer: dbl --> int
+# mmr: dbl --> int
+# sellingprice: dbl --> int
+
+df = df %>%
+  mutate(year = as.integer(year), 
+         condition = as.integer(condition),
+         odometer = as.integer(odometer),
+         mmr = as.integer(mmr),
+         sellingprice = as.integer(sellingprice))
